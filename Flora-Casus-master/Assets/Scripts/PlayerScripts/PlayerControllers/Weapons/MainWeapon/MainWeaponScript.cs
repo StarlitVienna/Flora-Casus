36031a7ba09af57dd5ef713eaf963b539afd53ab
starlitvienna <pseudonothing@outlook.com>
2023-11-02 12:00:08 -0300

    Added new map and OC main character

---
+++ Flora-Casus-master/Assets/Scripts/PlayerScripts/PlayerControllers/Weapons/MainWeapon/MainWeaponScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainWeaponScript : MonoBehaviour
+{
+    private float damage;
+
+    public Vector2 knockbackVector;
+    private Vector2 knockbackForce = new Vector2(8, 8);
+
+    private Collider2D weaponCollider;
+    private void Start()
+    {
+        damage = 3;
+        weaponCollider = gameObject.GetComponent<Collider2D>();
+        weaponCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+            enemy.takeDamage(damage);
+            enemy.takeKnockback(knockbackVector * knockbackForce / enemy.rb.mass);
+            
+        }
+    }
+
+    public void attackEnable()
+    {
+        weaponCollider.enabled = true;
+    }
+
+    public void attackDisable()
+    {
+        weaponCollider.enabled = false;
+    }
+}
---
+++ Flora-Casus-master/translations/readmes/english/README.md
@@ -0,0 +1,12 @@
+<div align='center'>
+
+<h1>Flora Casus</h1>
+<a href="https://github.com/StarlitVienna/Flora-Casus/blob/master/translations/readmes/english/README.md"> English </a>
+<p>Flora Casus é um jogo sobre um espírito da floresta, cuja missão é ajudar a natureza a enfrentar os problemas da ação antrópica causados no meio-ambiente</p>
+
+<h4> <span> · </span> <a href="https://github.com/StarlitVienna/Flora-Casus/blob/master/README.md"> Documentation </a> <span> · </span> <a href="https://github.com/StarlitVienna/Flora-Casus/issues"> Report a bug </a> <span> · </span> <a href="https://github.com/StarlitVienna/Flora-Casus/issues"> Request a feature </a> </h4>
+
+
+</div>
+
+## :star2: About the Project
